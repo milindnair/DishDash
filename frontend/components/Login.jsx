a7d1494872ea23551f1212ff2fcc5c92821ad305
Milind Nair <milindnair261003@gmail.com>
2023-04-22 20:32:56 +0530

    added initial files for login and sign up page

---
+++ frontend/components/Login.jsx
@@ -0,0 +1,56 @@
+import image from "../src/login_image1.jpg";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+        <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+          <div className="md:w-1/2 px-8 md:px-16">
+            <h2 className="font-bold text-4xl text-[#002D74]">Dish Up</h2>
+            <p className="text-xl mt-4 text-[#002D74]">
+                Welcome back!<br />
+                Please login to your account.
+            </p>
+
+            <form action="" className="flex flex-col gap-4">
+              <input
+                className="p-2 mt-8 rounded-xl border"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+              <div className="relative">
+                <input
+                  className="p-2 rounded-xl border w-full"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+              </div>
+              <button className="bg-[#ea291a] rounded-xl text-white py-2 hover:scale-105 duration-300">
+                Login
+              </button>
+            </form>
+            <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+              <a href="#">Forgot your password?</a>
+            </div>
+
+            <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+              <p>Don't have an account?</p>
+              <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
+                <Link to="/register">Register</Link>
+              </button>
+            </div>
+          </div>
+
+          <div className="md:block hidden w-1/2">
+            <img className=" rounded-2xl" src={image} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+import Login from '../components/Login';
+import Register from '../components/Register';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+function App() {
+
+  return (
+    <>
+      <Router>
+      <Routes>        
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </Router>
+      
+    </>
+  )
+}
+
+export default App
