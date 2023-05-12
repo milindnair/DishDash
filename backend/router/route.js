ec7e1257dac2563c7bd011d3486b9fe140035ccf
Milind Nair <milindnair261003@gmail.com>
2023-05-12 19:16:53 +0530

    Added mail api

--- backend/router/route.js
+++ backend/router/route.js
@@ -4,15 +4,14 @@ const router = Router();
 /** import all controllers */
 import * as controller from '../controllers/appController.js';
 import {Auth,localVariables} from '../middleware/auth.js';
-// import { registerMail } from '../controllers/mailer.js'
-// import Auth, { localVariables } from '../middleware/auth.js';
+import { registerMail } from '../controllers/mailer.js';
 
 
 
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
-// router.route('/registerMail').post(registerMail); // send the email
-// router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
+router.route('/registerMail').post(registerMail); // send the email
+router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
 router.route('/login').post(controller.verifyUser,controller.login); // login in app
 
 /** GET Methods */
